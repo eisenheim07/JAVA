771ccdc0a83734b4e9269eda35ee1f40c0b8a450
eisenheim <azameisenheim07@gmail.com>
2024-12-08 23:39:23 +0530

    JAVA

---
+++ AbstractClass.java
@@ -0,0 +1,46 @@
+/*ABSTRACTION EXAMPLE.*/
+
+package practice;
+
+abstract class Abstraction {
+
+	int abstractVar;
+	abstract void start();
+
+}
+
+class First extends Abstraction {
+
+	@Override
+	void start() {
+		System.out.println("FIRST");
+	}
+	
+}
+
+class Second extends Abstraction {
+
+	@Override
+	void start() {
+		System.out.println("SECOND");
+	}
+
+}
+
+public class AbstractClass {
+
+	public static void main(String[] args) {
+		Abstraction obj;	/* REFERENCE VARIABLE */
+		
+		obj = new First();
+		obj.abstractVar = 10;
+		obj.start();
+		System.out.println(obj.abstractVar);
+
+		obj = new Second();
+		obj.abstractVar = 20;
+		obj.start();
+		System.out.println(obj.abstractVar);
+	}
+
+}
