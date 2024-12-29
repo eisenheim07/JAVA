7fc0fde1206d568ed45af06782c8bf3c416deef8
eisenheim <azameisenheim07@gmail.com>
2024-12-29 16:09:49 +0530

    JAVAC

---
+++ diamondProblem/Car.java
@@ -0,0 +1,34 @@
+package javac.diamondProblem;
+
+public interface Car {
+    default void drive() {
+        System.out.println("CAR DRIVE METHOD");
+    }
+}
+
+interface PetrolCar extends Car {
+    @Override
+    default void drive() {
+        System.out.println("PETROL CAR DRIVE METHOD");
+    }
+}
+
+interface ElectricCar extends Car {
+    @Override
+    default void drive() {
+        System.out.println("ELECTRIC CAR DRIVE METHOD");
+    }
+}
+
+class HybridCar implements PetrolCar, ElectricCar {
+
+    @Override
+    public void drive() {
+        PetrolCar.super.drive();
+        ElectricCar.super.drive();
+    }
+
+    public static void main(String[] args) {
+        new HybridCar().drive();
+    }
+}
